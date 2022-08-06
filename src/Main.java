a33e54d76fe1c6f6f374bf795e06378d2429109a
Kisnata <nata.van4inova@yandex.ru>
2022-08-06 10:54:57 +0300

    Урок условные операторы

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 6;
+        if (age == 18) {
+            System.out.println("С окончанием школы");
+        } else if (age == 21) {
+            System.out.println("Теперь тебе можно пить алкоголь");
+        } else if (age == 7) {
+            System.out.println("Иди в школу");
+        } else {
+            System.out.println("Пока сказать нечего");
+        }
+    }
+}
